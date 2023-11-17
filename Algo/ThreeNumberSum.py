fe258cccf3bc17dadfa599971ad4a4bbfadc284b
VrajMalvi <66006412+VrajMalvi@users.noreply.github.com>
2023-11-17 17:31:32 -0500

    update

--- Algo/ThreeNumberSum.py
+++ Algo/ThreeNumberSum.py
@@ -1,4 +1,7 @@
-# return all possible combinations of 3 element that results in target sum in acending order
+# return all possible combinations of 3 element that results in target sum
+# in acending order
+
+# Solution 1 :
 def threeNumberSum(array, targetSum):
     # Write your code here.
     array.sort()
@@ -19,3 +22,24 @@ def threeNumberSum(array, targetSum):
                 right -= 1
         current += 1
     return output
+
+# Solution 2 :
+def threeNumberSum(array, targetSum):
+    # Write your code here.
+    array.sort()
+    triplets = []
+    for i in range(len(array)-2):
+        # in case last three element are target sum we need to have two more elements after that so -2
+        left = i + 1
+        right = len(array)-1
+        while (left < right):
+            currentsum = array[i] + array[left] + array[right]
+            if targetSum == currentsum:
+                triplets.append([array[i], array[left], array[right]])
+                left += 1
+                right -= 1
+            elif currentsum < targetSum:
+                left += 1
+            else:
+                right -= 1
+    return triplets
