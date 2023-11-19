acb443bc9c5e26a2e80f3b0c5ee27ff276d4e60c
VrajMalvi <66006412+VrajMalvi@users.noreply.github.com>
2023-11-19 17:59:40 -0500

    update

--- Machine Learning Algorithms/distance_and_sililarities.py
+++ Machine Learning Algorithms/distance_and_sililarities.py
@@ -14,31 +14,36 @@
 # Sol
 
 from math import sqrt
+
+
 class Metrics():
-   def euclidean_distance(self, X, Y):
-       # Write your code here.
-       return sqrt(sum(((x - y) ** 2) for x,y in zip(X,Y)))
+    def euclidean_distance(self, X, Y):
+        # Write your code here.
+        return sqrt(sum(((x - y) ** 2) for x, y in zip(X, Y)))
 
-   def manhattan_distance(self, X, Y):
-       # Write your code here.
-       return sum(abs(x - y) for x,y in zip(X,Y)) 
+    def manhattan_distance(self, X, Y):
+        # Write your code here.
+        return sum(abs(x - y) for x, y in zip(X, Y))
 
-   def cosine_similarity(self, X, Y):
-       # Write your code here.
-       numerator =  sum((x * y) for x,y in zip(X,Y)) 
-       denominator = sqrt(sum([x * x for x in X])) * sqrt(sum([y * y for y in Y]))
-       return numerator / float(denominator)
+    def cosine_similarity(self, X, Y):
+        # Write your code here.
+        numerator = sum((x * y) for x, y in zip(X, Y))
+        denominator = sqrt(sum([x * x for x in X])) * \
+            sqrt(sum([y * y for y in Y]))
+        return numerator / float(denominator)
 
-   def jaccard_similarity(self, X, Y):
-       # Write your code here.
-       intersecation_cardinality = len(set.intersection(*[set(X), set(Y)]))
-       union_cardinality = len(set.union(*[set(X), set(Y)]))
-       return intersecation_cardinality / float(union_cardinality)
+    def jaccard_similarity(self, X, Y):
+        # Write your code here.
+        intersecation_cardinality = len(set.intersection(*[set(X), set(Y)]))
+        union_cardinality = len(set.union(*[set(X), set(Y)]))
+        # or we can return:
+        # return len(set(x).intersection(set(y))) / len(set(x).union(set(y)))
+        return intersecation_cardinality / float(union_cardinality)
 
 
 def distances_and_similarities(X, Y):
-   metrics = Metrics()
-   return [metrics.euclidean_distance(X, Y),
-           metrics.manhattan_distance(X, Y),
-           metrics.cosine_similarity(X, Y),
-           metrics.jaccard_similarity(X, Y)]
+    metrics = Metrics()
+    return [metrics.euclidean_distance(X, Y),
+            metrics.manhattan_distance(X, Y),
+            metrics.cosine_similarity(X, Y),
+            metrics.jaccard_similarity(X, Y)]
--- Machine Learning Models from scratch/KNN.py
+++ Machine Learning Models from scratch/KNN.py
@@ -1,37 +1,40 @@
-# we are building knn with only in built library 
+# we are building knn with only in built library
 # equestion for Euclidean distance : d = √((x1 - x2)^2 + (y1 - y2)^2 + (z1 - z2)^2 + ... + (xn - xn)^2)
 
 import numpy as np
 from collections import Counter
+
 
 def euclidean_distance(x1, x2):
     return np.sqrt(np.sum((x1 - x2) ** 2))
 
 
 class KNN:
-    
+
     def __init__(self, k=3):
         '''Initialize nearest neighbours, by default its 3'''
         self.k = k
-        
+
     def fit(self, X, y):
         self.X_train = X
         self.y_train = y
-    
+
     def predict(self, X):
         predicted_labels = [self._predict(x) for x in X]
         return np.array(predicted_labels)
-        
+
     def _predict(self, x):
-        #compute distance 
-        distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
-        # get k nearest samples, lables 
+        # compute distance
+        distances = [euclidean_distance(x, x_train)
+                     for x_train in self.X_train]
+        # get k nearest samples, lables
         k_indices = np.argsort(distances)[:self.k]
         k_nearesr_labels = [self.y_train[i] for i in k_indices]
         # majority vote, most common class lables
         most_common = Counter(k_nearesr_labels).most_common(1)
         return most_common[0][0]
-    
+
+
 if __name__ == "__main__":
     # Imports
     from matplotlib.colors import ListedColormap
